9b23aa651b507592cc81e0553985c11d1a662eb5
Yuanyi Xue <yxue@nyu.edu>
2013-03-21 14:40:45 -0400

    Minor changes on capture script
    Now capture log is in the external files and return ``all done'' after
    screen capturing ends.

--- FFMPEG.bat
+++ FFMPEG.bat
@@ -6,4 +6,5 @@ REM For any concern, please contact Fanyi via dmfynyu@gmail.com
 
 cd C:\\oovoo project\\ffmpeg\\bin
 del test.avi
-ffmpeg.exe -t 2400 -f dshow -i video="UScreenCapture" -r 25 -vcodec libx264 -tune zerolatency -b 4000k -pix_fmt yuv420p -rtbufsize 200MB captured.mp4
+ffmpeg.exe -t 2400 -f dshow -i video="UScreenCapture" -r 25 -vcodec libx264 -tune zerolatency -b 4000k -pix_fmt yuv420p -rtbufsize 200MB captured.mp4 > ffmpegCap.log
+REM All done!
