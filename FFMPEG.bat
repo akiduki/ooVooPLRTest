c321f0bf5b30fd195bc1b57e405eab5af2e588a1
Yuanyi Xued <yxue@nyu.edu>
2013-03-25 15:34:32 -0400

    Misc changes on scripts

--- FFMPEG.bat
+++ FFMPEG.bat
@@ -5,6 +5,6 @@ REM Last Update: 03/19/2013	Version: 2.0
 REM For any concern, please contact Fanyi via dmfynyu@gmail.com
 
 cd C:\\oovoo project\\ffmpeg\\bin
-del capture.mp4
+del captured.mp4
 ffmpeg.exe -t 2400 -f dshow -i video="UScreenCapture" -r 25 -vcodec libx264 -tune zerolatency -b 4000k -pix_fmt yuv420p -rtbufsize 200MB captured.mp4 2> ffmpegCap.log
 REM All done!
--- NEWT512.bat
+++ NEWT512.bat
@@ -6,26 +6,26 @@ REM For any concern, please contact Fanyi via dmfynyu@gmail.com
 
 cd C:\\Program Files\\Network Emulator for Windows Toolkit\\bin
 
-newtman /t 480 /e "Test" BW512kbps_BL1.xml 1> BW512kbl1.log
-echo "Burst Loss 1% for 300 sec"
+echo "Burst Loss 1% for 480 sec"
+newtman /t 480 /e "Test" BW512kbps_BL1.xml 2> BW512kbl1.log
 
-newtman /e "Test" /f "Test" /t 300 BW512kbps_BL3.xml 1> BW512bl3.log
 echo "Burst Loss 3% for 300 sec"
+newtman /e "Test" /f "Test" /t 300 BW512kbps_BL3.xml 2> BW512bl3.log
 
-newtman /e "Test" /f "Test" /t 300 BW512kbps_BL5.xml 1> BW512bl5.log
 echo "Burst Loss 5% for 300 sec"
+newtman /e "Test" /f "Test" /t 300 BW512kbps_BL5.xml 2> BW512bl5.log
 
-newtman /e "Test" /f "Test" /t 450 BW512kbps_BL10.xml 1> BW512bl10.log
 echo "Burst Loss 10% for 450 sec"
+newtman /e "Test" /f "Test" /t 450 BW512kbps_BL10.xml 2> BW512bl10.log
 
-newtman /e "Test" /f "Test" /t 300 BW512kbps_BL5.xml 1> BW512bl5down.log
 echo "Burst Loss 5% for 300 sec"
+newtman /e "Test" /f "Test" /t 300 BW512kbps_BL5.xml 2>BW512bl5down.log
 
-newtman /e "Test" /f "Test" /t 300 BW512kbps_BL3.xml 1> BW512bl3down.log
 echo "Burst Loss 3% for 300 sec"
+newtman /e "Test" /f "Test" /t 300 BW512kbps_BL3.xml 2> BW512bl3down.log
 
-newtman /e "Test" /f "Test" /t 300 BW512kbps_BL1.xml 1> BW512bl1down.log
 echo "Burst Loss 1% for 300 sec"
+newtman /e "Test" /f "Test" /t 300 BW512kbps_BL1.xml 2> BW512bl1down.log
 
-newtman /f "Test"
 echo "BINGO"
+newtman /f "Test"
